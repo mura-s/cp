1a97abfe503394511ea0769827ef3a28c0cbf841
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2019-05-18 01:10:44 +0900

    Initial commit

---
+++ atcoder/abc116/a/main.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  int ab, bc, ca;
+  cin >> ab >> bc >> ca;
+
+  cout << ab * bc / 2 << endl;
+  return 0;
+}
