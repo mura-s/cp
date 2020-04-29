f6969bb423689f25f647564ee866257be9e0e1f2
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-04-29 14:37:40 +0900

    update to clang++

---
+++ atcoder/abc164/c/main.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+typedef long long ll;
+// const int INF = (1 << 30) - 1;
+// const ll LL_INF = (1LL << 62) - 1;
+// const ll MOD = 1e9 + 7;
+
+int N;
+set<string> st;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    string si;
+    cin >> si;
+    st.insert(si);
+  }
+
+  int ans = st.size();
+  cout << ans << endl;
+  return 0;
+}
