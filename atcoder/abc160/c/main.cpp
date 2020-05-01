c92b6665a20fc252b71397b933a85000edf4c4f6
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-05-01 20:47:36 +0900

    add abc160/c

--- atcoder/abc160/c/main.cpp
+++ atcoder/abc160/c/main.cpp
@@ -13,25 +13,32 @@
 #include <vector>
 using namespace std;
 typedef long long ll;
-// const int INF = (1 << 30) - 1;
+const int INF = (1 << 30) - 1;
 // const ll LL_INF = (1LL << 62) - 1;
 // const ll MOD = 1e9 + 7;
 
-int N;
-// vector<int> A;
+int K, N;
+vector<int> A;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  cin >> N;
-  // A.resize(N);
-  // for (int i = 0; i < N; i++) {
-  //   int tmp;
-  //   cin >> tmp;
-  //   A[i] = tmp;
-  // }
+  cin >> K >> N;
+  A.resize(N);
+  for (int i = 0; i < N; i++) {
+    int tmp;
+    cin >> tmp;
+    A[i] = tmp;
+  }
 
-  int ans = 0;
+  int ans = INF;
+  for (int i = 0; i < N; i++) {
+    if (i == N - 1) {
+      ans = min(ans, K - abs(K - A[i]) - abs(A[0]));
+    } else {
+      ans = min(ans, K - abs(A[i + 1] - A[i]));
+    }
+  }
 
   cout << ans << endl;
   return 0;
