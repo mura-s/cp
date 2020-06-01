750caa0d7587a0d1a7f3435c3d4a58419e773256
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-06-01 14:48:40 +0900

    add java/mod_minus

---
+++ template/java/lib/mod_minus/Main.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Main {
+    // static int INF = (1 << 30) - 1;
+    // static long L_INF = (1L << 62) - 1;
+    static long MOD = (long) 1e9 + 7;
+
+    static class Solver {
+        // 負の数にも対応した MOD 演算
+        long mod(long val) {
+            long res = val % MOD;
+            if (res < 0)
+                res += MOD;
+            return res;
+        }
+
+        // write your code
+        void solve(InputReader in, PrintWriter out) {
+            long a = 2000000020;
+            long b = 20;
+            long ans = mod((a % MOD) - (b % MOD));
+            out.println(a % MOD);
+            out.println(b % MOD);
+            out.println(ans);
+        }
+
+        void debug(Object... os) {
+            System.err.println(Arrays.deepToString(os));
+        }
+    }
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
