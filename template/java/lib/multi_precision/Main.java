90ad2c0fd9a181cefc24bbd3f657f9db760b0184
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-06-01 15:07:56 +0900

    add java/multi_precisioln

---
+++ template/java/lib/multi_precision/Main.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.*;
+
+public class Main {
+    // static int INF = (1 << 30) - 1;
+    // static long L_INF = (1L << 62) - 1;
+    // static long MOD = (long) 1e9 + 7;
+
+    static class Solver {
+        // write your code
+        void solve(InputReader in, PrintWriter out) {
+            BigInteger a = new BigInteger("12345678901234567890");
+            BigInteger b = new BigInteger("2");
+            out.println(a.multiply(b));
+
+            // https://javazuki.com/articles/bigdecimal-usage.html
+            BigDecimal c = new BigDecimal("0.00001");
+            BigDecimal d = new BigDecimal("0.00001");
+            out.println(c.multiply(d).toPlainString());
+        }
+
+        void debug(Object... os) {
+            System.err.println(Arrays.deepToString(os));
+        }
+    }
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Solver solver = new Solver();
+        solver.solve(in, out);
+        out.close();
+    }
+
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
