1d3d860df38f01e6b9e138c05c5017130fd579d6
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-06-22 11:10:29 +0900

    add dpl_1/a

--- aoj/dpl_1/a/main.cpp
+++ aoj/dpl_1/a/main.cpp
@@ -19,22 +19,32 @@ typedef long double ld;
 typedef pair<int, int> P;
 typedef pair<ll, ll> LLP;
 
-// const int INF = (1 << 30) - 1;
+const int INF = (1 << 30) - 1;
 // const ll LL_INF = (1LL << 62) - 1;
 // const ll MOD = 1e9 + 7;
 // const double PI = M_PI;
 
-int N;
-// vector<int> A;
+int n, m;
+vector<int> c;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  cin >> N;
-  // A.resize(N);
-  // rep(i, N) { cin >> A[i]; }
+  cin >> n >> m;
+  c.resize(m);
+  rep(i, m) { cin >> c[i]; }
+  int dp[n + 1];
+  fill(dp, dp + n + 1, INF);
+  dp[0] = 0;
 
-  int ans = 0;
-  cout << ans << endl;
+  rep(i, m) {
+    rep(j, n + 1) {
+      if (dp[i] != INF && j + c[i] <= n) {
+        dp[j + c[i]] = min(dp[j + c[i]], dp[j] + 1);
+      }
+    }
+  }
+
+  cout << dp[n] << endl;
   return 0;
 }
