de5658a5a38816393204b49b4f44df8d179698d6
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-07-05 17:40:51 +0900

    add abc075/c

--- atcoder/abc075/c/main.cpp
+++ atcoder/abc075/c/main.cpp
@@ -1,22 +1,29 @@
 #include <algorithm>
 #include <cassert>
-#include <climits>
 #include <cmath>
 #include <functional>
 #include <iomanip>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <queue>
 #include <set>
+#include <stack>
 #include <string>
 #include <vector>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
 using namespace std;
 typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LLP;
 
-// ref. https://kira924age.github.io/Algorithm/data_structure/union_find.html
-//
-// 親がsizeのマイナス値を持つ実装もある:
-//   https://ei1333.github.io/luzhiled/snippets/structure/union-find.html
+// const int INF = (1 << 30) - 1;
+// const ll LL_INF = (1LL << 62) - 1;
+// const ll MOD = 1e9 + 7;
+// const double PI = M_PI;
+
 struct uf_tree {
   vector<int> par, sizes;
   uf_tree(int n) : par(n), sizes(n, 1) {
@@ -46,28 +53,33 @@ struct uf_tree {
   int size(int x) { return sizes[find(x)]; }
 };
 
+int N, M;
+vector<int> a, b;
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int n = 10;
-  // cin >> n;
-  uf_tree uf(n);
-
-  uf.unite(2, 3);
-  uf.unite(2, 4);
-
-  if (uf.same(3, 4)) {
-    cout << "Yes" << endl;
-  } else {
-    cout << "No" << endl;
+  cin >> N >> M;
+  a.resize(M);
+  b.resize(M);
+  rep(i, M) {
+    cin >> a[i] >> b[i];
+    a[i]--;
+    b[i]--;
   }
 
-  if (uf.same(2, 5)) {
-    cout << "Yes" << endl;
-  } else {
-    cout << "No" << endl;
+  int ans = 0;
+  rep(i, M) {
+    uf_tree ut(N);
+    rep(j, M) {
+      if (i != j) {
+        ut.unite(a[j], b[j]);
+      }
+    }
+    if (ut.size(0) != N) {
+      ans++;
+    }
   }
-
-  cout << uf.size(3) << endl;
+  cout << ans << endl;
   return 0;
 }
---
+++ atcoder/abc075/c/test.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+all_ac=true
+
+try() {
+    input="$1"
+    expected="$2"
+
+    actual="$(echo $input | ./main)"
+
+    if [ "$actual" != "$expected" ]; then
+        echo "WA"
+        echo "expected:"
+        echo "$expected"
+        echo
+        echo "actual:"
+        echo "$actual"
+        echo
+
+        all_ac=false
+        return
+    fi
+    echo "AC"
+    echo
+}
+
+# test cases
+echo "[Sample1]"
+input1="$(cat << EOS
+7 7
+1 3
+2 7
+3 4
+4 5
+4 6
+5 6
+6 7
+EOS
+)"
+expected1="$(cat << EOS
+4
+
+EOS
+)"
+try "$input1" "$expected1"
+
+echo "[Sample2]"
+input2="$(cat << EOS
+3 3
+1 2
+1 3
+2 3
+EOS
+)"
+expected2="$(cat << EOS
+0
+
+EOS
+)"
+try "$input2" "$expected2"
+
+echo "[Sample3]"
+input3="$(cat << EOS
+6 5
+1 2
+2 3
+3 4
+4 5
+5 6
+EOS
+)"
+expected3="$(cat << EOS
+5
+
+EOS
+)"
+try "$input3" "$expected3"
+
+# echo "[Sample4]"
+# input4="$(cat << EOS
+#
+# EOS
+# )"
+# expected4="$(cat << EOS
+#
+# EOS
+# )"
+# try "$input4" "$expected4"
+
+# echo "[Sample5]"
+# input5="$(cat << EOS
+#
+# EOS
+# )"
+# expected5="$(cat << EOS
+#
+# EOS
+# )"
+# try "$input5" "$expected5"
+
+echo "[Result]"
+if ! $all_ac; then
+    echo "WA"
+    echo
+    exit 1
+fi
+echo "All AC"
