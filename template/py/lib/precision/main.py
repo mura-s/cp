e485714cca8f09600c7fc05323d82b289a8f0378
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-11-17 00:08:27 +0900

    add py precision

---
+++ template/py/lib/precision/main.py
@@ -0,0 +1,11 @@
+import bisect
+import collections
+import itertools
+import math
+import sys
+from copy import deepcopy
+from heapq import heappop, heappush
+from operator import itemgetter
+
+ans = 10.000001
+print('{:.9f}'.format(ans))
