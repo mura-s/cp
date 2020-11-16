b920fe3619fc02c975839e4b8180277d44318b50
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-11-17 00:38:09 +0900

    add py next_permutation

---
+++ template/py/lib/next_permutation/main.py
@@ -0,0 +1,13 @@
+import bisect
+import collections
+import itertools
+import math
+import sys
+from copy import deepcopy
+from decimal import Decimal
+from heapq import heappop, heappush
+from operator import itemgetter
+
+l = [1, 2, 3, 4]
+for x in itertools.permutations(l):
+    print(x)
