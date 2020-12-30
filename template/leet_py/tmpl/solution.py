433fa12589b062ab24fe917dd7f0e455a56216e8
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2020-12-30 20:39:01 +0900

    add leet_py

---
+++ template/leet_py/tmpl/solution.py
@@ -0,0 +1,19 @@
+import bisect
+import collections
+import itertools
+import math
+import sys
+from copy import deepcopy
+from decimal import Decimal
+from heapq import heappop, heappush
+from operator import itemgetter
+
+sys.setrecursionlimit(10**6)
+# INF = (1 << 30) - 1
+# LL_INF = (1 << 62) - 1
+# MOD = 10**9 + 7
+
+
+class Solution:
+    def ans(self) -> None:
+        return
---
+++ template/leet_py/tmpl/test_solution.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from solution import Solution
+
+
+class TestSolution(TestCase):
+    def test_ans1(self):
+        self.assertEqual(Solution().ans(), None)
+
+    def test_ans2(self):
+        self.assertEqual(Solution().ans(), None)
+
+    def test_ans3(self):
+        self.assertEqual(Solution().ans(), None)
